59831a6fd260b05eeae596e3216941259f9e3afb
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-04-29 18:41:12 -0400

    cleanup

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const morgan = require ('morgan');
 const fs = require('fs');
 // var md5 = require('md5');
 // const { restart } = require('nodemon');
-
 //read unrlencoded and json using express
 app.use(express.urlencoded({extended: true}));
 // Make Express use its own built-in body parser to handle JSON
